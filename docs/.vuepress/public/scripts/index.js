3426faa50eded1c582ec55d36d3fbe13688faa33
王小泽 <wangze@anbangke.com>
2024-04-26 12:00:23 +0800

    feat: 订阅发布

--- docs/.vuepress/public/scripts/index.js
+++ docs/.vuepress/public/scripts/index.js
@@ -48,4 +48,16 @@ class Utils extends Dom {
 	}
 }
 
+function baiduTotal() {
+	var _hmt = _hmt || [];
+	(function () {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?7788821213245c8545569fd14a3fc292";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();
+}
+
+baiduTotal();
+
 const utils = new Utils();
