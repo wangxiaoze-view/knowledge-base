207eb8f6f5760064a623e48a14519299727b13c9
wangxiaoze <wangxiaoze@petalmail.com>
2024-06-09 18:48:26 +0800

    log: 刪除log

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -9,13 +9,13 @@ export default ({
 	isServer, // 当前应用配置是处于 服务端渲染 还是 客户端
 }) => {
 	router.beforeEach((to, from, next) => {
-		console.log("切换路由", to.fullPath, from.fullPath);
+		// console.log("切换路由", to.fullPath, from.fullPath);
 
 		//触发百度的pv统计
 		if (typeof _hmt != "undefined") {
 			if (to.path) {
 				_hmt.push(["_trackPageview", to.fullPath]);
-				console.log("上报百度统计", to.fullPath);
+				// console.log("上报百度统计", to.fullPath);
 			}
 		}
 
