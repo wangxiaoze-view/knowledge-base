057896c933882fda785155472d6e05b666cc6eb3
王小泽 <wangxiaoze@petalmail.com>
2024-10-08 18:12:16 +0800

    feat: 图片懒加载效果

--- docs/demos/js/windowCross/components/cross1.vue
+++ docs/demos/js/windowCross/components/cross1.vue
@@ -1,7 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+const list = new Array(100).fill(0);
+onMounted(() => {
+	const ob = new IntersectionObserver(
+		entries => {
+			for (const element of entries) {
+				const target = element.target as HTMLImageElement;
+				if (element.isIntersecting) {
+					target.src = target.getAttribute("data-src")!;
+					ob.unobserve(target);
+				}
+			}
+		},
+		{
+			root: document.querySelector(".cross-1"),
+			rootMargin: "0px",
+			threshold: 1,
+		}
+	);
+	document.querySelectorAll(".cross-1 img").forEach(img => {
+		ob.observe(img);
+	});
+});
+</script>
 
 <template>
-	<div class="cross-1"></div>
+	<div class="cross-1">
+		<div v-for="(_, index) in list" :key="index">
+			<img
+				src="https://file.wangzevw.com/libs/remote-components/icons/404.svg"
+				:data-src="`https://picsum.photos/200/300?random=${Math.random()}`"
+				alt=""
+			/>
+		</div>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cross-1 {
+	width: 100%;
+	height: 400px;
+	overflow-y: auto;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 20px;
+}
+</style>
--- docs/demos/js/windowCross/index.md
+++ docs/demos/js/windowCross/index.md
@@ -5,7 +5,7 @@ outline: deep
 敬请期待
 
 ## 图片懒加载
-
+<preview path="./components/cross1.vue"></preview>
 ## 滚动加载
 
 ## 视频广告
