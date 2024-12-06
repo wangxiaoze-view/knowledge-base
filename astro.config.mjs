d93e2292d8194ddf80f5fd98c0cd76eebf2abd98
王小泽 <wangxiaoze@petalmail.com>
2024-12-06 17:50:01 +0800

    init: 基础布局

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), vue(), tailwind()],
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), vue(), tailwind(), partytown()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,28 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE = {
+	author: "Wangxiaoze",
+	url: "https://wangzevw.com",
+	title: "知识库",
+	description: "一个记录知识的地方",
+};
+
+export const SETTINGS = {
+	// google 分析
+	GoogleAnalytics: {
+		enable: true,
+		id: "GTM-KJ452JQX",
+	},
+
+	// Umami 分析
+	UmamiAnalytics: {
+		enable: true,
+		dataWebsiteID: "63e283e0-9946-4f12-a732-c0e74a121289",
+	},
+
+	// 平滑滚动
+	lenis: {
+		enable: true,
+	},
+};
